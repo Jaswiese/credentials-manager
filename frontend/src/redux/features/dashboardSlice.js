c9b30a6be932762e136c537f1a393c8cdbb100a4
Jaswiese <jasper.wiese@protonmail.com>
2022-03-15 00:39:03 +0200

    added commets to the dashboardSlice

--- frontend/src/redux/features/dashboardSlice.js
+++ frontend/src/redux/features/dashboardSlice.js
@@ -1,10 +1,16 @@
 import { createSlice } from '@reduxjs/toolkit';
-
+// default state declared
 const initialState = {
   addComponent: false,
   viewUsers: false,
 };
-
+/**
+ * dashboardSlice declared
+ * state named dashboard
+ * reducers:
+ * toggleAdd sets the AddComponent state to the opposite when called
+ * toggleUsers sets the ViewUsers state to the opposite when called
+ */
 export const dashboardSlice = createSlice({
   name: 'dashboard',
   initialState,
@@ -17,7 +23,7 @@ export const dashboardSlice = createSlice({
     },
   },
 });
-
+// slice actions exported
 export const { toggleAdd, toggleUsers } = dashboardSlice.actions;
-
+// export slice reducer
 export default dashboardSlice.reducer;
