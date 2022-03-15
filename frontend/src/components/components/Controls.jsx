bc2ddaa1dced2d7382bc81d40a8edaea2b4f244e
Jaswiese <jasper.wiese@protonmail.com>
2022-03-15 03:12:55 +0200

    Controls component comments added

--- frontend/src/components/components/Controls.jsx
+++ frontend/src/components/components/Controls.jsx
@@ -4,17 +4,26 @@ import { useDispatch, useSelector } from 'react-redux';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+// toggleAdd redux action imported
 import { toggleAdd } from '../../redux/features/dashboardSlice';
-
+/**
+ * Controls component
+ * @returns a button for adding credentials
+ */
 function Controls() {
   // dispatch assigned the value of the useDispatch hook
   const dispatch = useDispatch();
-
+  // useSelector hook used to sync with global state
   const viewCredentials = useSelector((state) => state.dashboard.addComponent);
-
+  // handleAddCredential fires the toggleAdd action
   const handleAddCredential = () => {
     dispatch(toggleAdd());
   };
+  /**
+   * bootstrap components are used for layout
+   * add credentials button added with onClick event that fires the handleAddCredential function
+   * the buttons text also changes with its state, when in the add credential state, viewCredentials is displayed
+   */
   return (
     <Container fluid>
       <Row>
